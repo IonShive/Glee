293f12e362ea2d94caef60676cc1e4b3ce601cf4
IonShive <vlad.balaban2020@gmail.com>
2021-04-19 17:22:43 +0300

    added product detalis

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,29 @@
 $(function () {
 
+	$('.product-detalis__slider-thumb').slick({
+		asNavFor: '.product-detalis__slider-big',
+		focusOnSelect: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		vertical: true,
+		draggable: false,
+	});
+
+	$('.product-detalis__slider-big').slick({
+		asNavFor: '.product-detalis__slider-thumb',
+		draggable: false,
+		arrows: false,
+		fade: true,
+		responsive: [
+			{
+				breakpoint: 540,
+				settings: {
+					draggable: true,
+				}
+			},
+		]
+	});
+
 	$('.product__filter-btn').on('click', function () {
 		$('.product__filters').slideToggle();
 	});
@@ -12,6 +36,17 @@ $(function () {
 		$(this).next().slideToggle();
 		$(this).toggleToggle('active');
 	});
+
+	$('.product-info__item-num').styler();
+
+	$('.product-tabs__top-item').on('click', function (e) {
+		e.preventDefault();
+		$('.product-tabs__top-item').removeClass('product-tabs__top-item--active')
+		$(this).addClass('product-tabs__top-item--active')
+
+		$('.product-tabs__content-item').removeClass('product-tabs__content-item--active')
+		$($(this).attr('href')).addClass('product-tabs__content-item--active')
+	})
 
 	$(".star").rateYo({
 		normalFill: "#d6d6d6",
